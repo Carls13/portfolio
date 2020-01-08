1be62d1184b2b305759907b4c84d397671872555
Carls13 <charlesshb98@gmail.com>
2020-01-07 20:10:30 -0400

    Deploy port to Heroku

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const app = next({dev: false})
 const handler = routes.getRequestHandler(app)
 
 const port = process.env.PORT || 3000
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 // Without express
 const {createServer} = require('http')
 app.prepare().then(() => {
-  createServer(handler).listen(port)
+  createServer(handler).listen(server_port, server_host)
 })
