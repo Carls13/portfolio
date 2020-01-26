3e65f4a171df409baea76bd032a25bc8faae0fc2
Carls13 <charlesshb98@gmail.com>
2020-01-26 13:34:23 -0400

    Serving serviceWorker

--- server.js
+++ server.js
@@ -9,8 +9,22 @@ const port = process.env.PORT || 3000;
 
 // Without express
 const {createServer} = require('http')
+const { join } = require('path')
+const { parse } = require('url')
+
 app.prepare().then(() => {
-  createServer(handler).listen(port, host, function() {
+	createServer((req, res) => {
+		const parsedUrl = parse(req.url, true)
+		const { pathname } = parsedUrl
+  
+		// handle GET request to /service-worker.js
+		if (pathname === '/service-worker.js') {
+		  const filePath = join(__dirname, '.next', pathname)
+		  app.serveStatic(req, res, filePath)
+		} else {
+		  handler(req, res, parsedUrl)
+		}
+	  }).listen(port, host, function() {
   	console.log("Server started.......");
 	});
 });
